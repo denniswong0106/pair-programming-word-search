b464492c2a35d22923b090a1d90a32d700c42108
Dennis Wong <denniswong0106@gmail.com>
2021-03-11 01:43:11 +0000

    added functionality to wordsearch to search verticle + horizontal, no diagonal or backwards functional yet

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    //const transposedLet = letters[0].map((col, i) => letters.map(row => row[i]));
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) {
+      return true;
+      }
     }
-}
+
+  // letters is an array of arrays, each array is one letter
+  let verticleArr = [];
+  for (let c = 0; c < letters[0].length; c++) {
+    let insideArr = []; 
+    for (let r = 0; r < letters.length; r++) {
+      insideArr.push(letters[r][c]);
+    }
+  verticleArr.push(insideArr);
+  }
+
+  const verticleJoin = verticleArr.map(ls => ls.join(''));
+  for (const c of verticleJoin) {
+    if (c.includes(word)) {
+        return true;
+    }
+  }
+  return false;
+};
+console.log(wordSearch([
+  ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+  ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+  ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+  ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+], 'FRANK'));
+
+console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'SEINFELD'));
+
 
 module.exports = wordSearch
